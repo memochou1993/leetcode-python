037930dec758f8c91bacbfd95f5a1f7a610a68cd
Memo Chou <memochou1993@gmail.com>
2024-07-30 02:37:21 +0800

    Fix solution

--- 0200.number-of-islands/main.py
+++ 0200.number-of-islands/main.py
@@ -5,7 +5,7 @@ class Solution:
         count = 0
 
         def dfs(grid: List[List[str]], i: int, j: int):
-            stack = []
+            stack = [(i, j)]
             while stack:
                 i, j = stack.pop()
                 if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[i]):
@@ -13,10 +13,10 @@ class Solution:
                 if grid[i][j] == '0' or grid[i][j] == 'V':
                     continue
                 grid[i][j] = 'V'
-                stack.append((i+1, j))
                 stack.append((i-1, j))
-                stack.append((i, j+1))
                 stack.append((i, j-1))
+                stack.append((i+1, j))
+                stack.append((i, j+1))
 
         for i in range(len(grid)):
             for j in range(len(grid[i])):
